de46a13cc3e4ef3c8e2af7861c117e98ec190784
Daniel Jennings <djenningsais@gmail.com>
2017-06-08 06:34:07 -0500

    +Initial commit for ShootHoop for Cardboard

---
+++ ShootHoop_Cardboard/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    ScoreKeeper score;
+    Text scoreText; 
+
+	// Use this for initialization
+	void Start () {
+        score = FindObjectOfType<ScoreKeeper>();
+        scoreText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        UpdateScoreText();        
+	}
+
+    void UpdateScoreText()
+    {        
+        scoreText.text = "Score: " + score.score;
+    }
+
+    
+
+    public void UpdateFinalScoreText()
+    {
+        scoreText.text = "Your Final Score is: " + score;
+    }
+}
